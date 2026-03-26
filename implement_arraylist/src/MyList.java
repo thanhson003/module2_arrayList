f985c4c6dcf308b657c18d2d010562f979c47ea1
thanhson003 <nthanhson0404@gmail.com>
2026-03-26 17:06:20 +0700

    26/03

---
+++ implement_arraylist/src/MyList.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {
+        elements = new Object[capacity];
+    }
+
+    private void ensureCapacity(int i) {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(int index, E element) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+    }
+
+    private void addFirst(E e) {
+    }
+
+}
